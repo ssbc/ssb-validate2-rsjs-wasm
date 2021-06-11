1051bbf09579e670499e43453a3d56f6a653c6cf
Andre Staltz <andre@staltz.com>
2021-06-11 18:29:30 +0300

    replace Jasmine with Mocha, refactor tests as mjs

---
+++ test/data/valid.js
@@ -0,0 +1,19 @@
+export default {
+  key: "%kmXb3MXtBJaNugcEL/Q7G40DgcAkMNTj3yhmxKHjfCM=.sha256",
+  value: {
+    previous: "%IIjwbJbV3WBE/SBLnXEv5XM3Pr+PnMkrAJ8F+7TsUVQ=.sha256",
+    author: "@U5GvOKP/YUza9k53DSXxT0mk3PIrnyAmessvNfZl5E0=.ed25519",
+    sequence: 8,
+    timestamp: 1470187438539,
+    hash: "sha256",
+    content: {
+      type: "contact",
+      contact: "@ye+QM09iPcDJD6YvQYjoQc7sLF/IFhmNbEqgdzQo3lQ=.ed25519",
+      following: true,
+      blocking: false,
+    },
+    signature:
+      "PkZ34BRVSmGG51vMXo4GvaoS/2NBc0lzdFoVv4wkI8E8zXv4QYyE5o2mPACKOcrhrLJpymLzqpoE70q78INuBg==.sig.ed25519",
+  },
+  timestamp: 1571140551543,
+};
